67817b29fb99851cee8618b434ebdda7be517bb7
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-01-19 08:59:29 -0800

    First commit

---
+++ data/data_prep.R
@@ -0,0 +1,18 @@
+# data-raw/mydataset.R
+# Data import and processing pipeline
+
+#manual_normals_1981t2010 <- read.csv(file = "./data/manual_normals.csv")
+#ASWE_normals_1981t2010 <- read.csv(file = "./data/ASWE_normals.csv")
+
+# Data cleaning code here...
+# (Do NOT put data analysis code here!)
+
+# This should be the last line.
+# Note that names are unquoted.
+# I like using overwrite = T so everytime I run the script the
+# updated objects are saved, but the default is overwrite = F
+#usethis::use_data(manual_normals_1981t2010, ASWE_normals_1981t2010, overwrite = T, internal = FALSE)
+
+
+#meta <- read.csv(file = "./data/station_metadata.csv")
+#usethis::use_data(meta, overwrite = T, internal = FALSE)
