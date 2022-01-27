cd6e64ca55d06c20c10dc61ef959951b785d443c
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-01-27 15:41:32 -0800

    revise to new bcsnowstats package

--- R/snow_stats_function.R
+++ R/snow_stats_function.R
@@ -33,7 +33,7 @@ snow_stats <- function(data, normal_min, normal_max, data_id, force = FALSE) {
   # get historic dataset - previous to this year
   df_hist <- data %>%
     dplyr::filter(wr < current_wy) %>% # Filter out the current year data prior to calculating statistics
-    dplyr::group_by(station_id, m_d) %>% # Group by the station ID and the month/day
+    dplyr::group_by(id, m_d) %>% # Group by the station ID and the month/day
     dplyr::rename(values_stats = all_of(data_id)) %>% # The user can define what data to run the statistics on. Usually this is the daily mean
     dplyr::filter(!is.na(values_stats)) # filter out missing data
 
@@ -49,67 +49,67 @@ snow_stats <- function(data, normal_min, normal_max, data_id, force = FALSE) {
                         dplyr::summarise(df_hist, Q90 = quantile(values_stats,0.90, na.rm = TRUE), .groups = "keep"),
                         dplyr::summarise(df_hist, max = max(values_stats, na.rm = TRUE), .groups = "keep"))) %>%
     dplyr::select(-m_d.1, -m_d.2, -m_d.3, -m_d.4, -m_d.5, -m_d.6, -m_d.7, -m_d.8) %>%
-    dplyr::select(-station_id.1, -station_id.2, -station_id.3, -station_id.4, -station_id.5, -station_id.6, -station_id.7, -station_id.8)
+    dplyr::select(-id.1, -id.2, -id.3, -id.4, -id.5, -id.6, -id.7, -id.8)
 
   # -------------------------------
   # Get the max, min dates, as well as the data range and number of years
   # Number of years. Implement threshold
   df_time <- data %>%
     dplyr::ungroup() %>%
-    dplyr::group_by(station_id) %>%
+    dplyr::group_by(id) %>%
     dplyr::filter(!is.na(value)) # filter out NA values to get more accurate start and end dates
 
   # Get the years that have >80% of data coverage during the snow accumulation period! Oct 1 - June 30
   df_time_2 <- df_time %>%
-    dplyr::arrange(station_id, date_utc) %>%
+    dplyr::arrange(id, date_utc) %>%
     dplyr::ungroup() %>%
-    dplyr::group_by(station_id, wr) %>%
+    dplyr::group_by(id, wr) %>%
     dplyr::filter(m_d >= "10-01" | m_d <= "06-30") %>%
     dplyr::mutate(percent_available = length(mean_day) / as.numeric(abs(difftime(as.POSIXct("2018-10-01"), as.POSIXct("2019-06-30"), units = "days")))*100) %>%
-    dplyr::select(station_id, wr, percent_available) %>%
+    dplyr::select(id, wr, percent_available) %>%
     unique() %>%
     dplyr::filter(wr == bcsnowdata::wtr_yr(Sys.Date()) | percent_available >= 50)
 
   daterange <- df_time_2 %>%
     dplyr::ungroup() %>%
-    dplyr::group_by(station_id) %>%
+    dplyr::group_by(id) %>%
     dplyr::summarize(maxdate = max(wr), mindate = min(wr), .groups = "keep") %>%
     dplyr::mutate(data_range = (paste0(mindate, " to ", maxdate)))
 
   numberyears <- df_time_2 %>%
     dplyr::ungroup() %>%
-    dplyr::group_by(station_id) %>%
+    dplyr::group_by(id) %>%
     dplyr::filter(wr != bcsnowdata::wtr_yr(Sys.Date())) %>%
     unique() %>%
     dplyr::mutate(numberofyears = length(percent_available)) %>%
-    dplyr::select(station_id, numberofyears) %>%
+    dplyr::select(id, numberofyears) %>%
     unique()
 
   # Bind stats regarding the number of years available together
-  df_time <- dplyr::full_join(daterange, numberyears, by = c("station_id"))
+  df_time <- dplyr::full_join(daterange, numberyears, by = c("id"))
 
   # Bind to the statistics
-  df_stat_date <- dplyr::full_join(df_stat, df_time, by = c("station_id"))
+  df_stat_date <- dplyr::full_join(df_stat, df_time, by = c("id"))
 
   # get the day of the max and min!!
   min_date <- df_hist %>%
     dplyr::ungroup() %>%
-    dplyr::group_by(station_id, m_d) %>%
+    dplyr::group_by(id, m_d) %>%
     dplyr::slice(which.min(values_stats)) %>%
-    dplyr::select(date_utc, station_id, m_d) %>%
+    dplyr::select(date_utc, id, m_d) %>%
     dplyr::rename(date_min_utc = date_utc)
 
   # Get the date of the max value for the date
   max_date <- df_hist %>%
     dplyr::ungroup() %>%
-    dplyr::group_by(station_id, m_d) %>%
+    dplyr::group_by(id, m_d) %>%
     dplyr::slice(which.max(values_stats)) %>%
-    dplyr::select(date_utc, station_id, m_d) %>%
+    dplyr::select(date_utc, id, m_d) %>%
     dplyr::rename(date_max_utc = date_utc)
 
   # append to data
-  dates <- dplyr::full_join(min_date, max_date, by = c("station_id", "m_d"))
-  df_stat_1 <- dplyr::full_join(df_stat_date, dates, by = c("station_id", "m_d"))
+  dates <- dplyr::full_join(min_date, max_date, by = c("id", "m_d"))
+  df_stat_1 <- dplyr::full_join(df_stat_date, dates, by = c("id", "m_d"))
 
   ### Calculate normals using function.
   # Function contains all of the thresholds, etc that make it
@@ -120,14 +120,14 @@ snow_stats <- function(data, normal_min, normal_max, data_id, force = FALSE) {
 
   # append the daily mean SWE to a column so that the percentile can be easily calculated within the SBI function
   df_hist_SWE <- df_hist %>%
-    dplyr::group_by(station_id, m_d) %>%
-    dplyr::select(m_d, values_stats, station_id) %>%
+    dplyr::group_by(id, m_d) %>%
+    dplyr::select(m_d, values_stats, id) %>%
     dplyr::rename(mean_SWE = values_stats) %>%
     tidyr::nest() %>%
-    dplyr::select(m_d, data, station_id)
+    dplyr::select(m_d, data, id)
 
   # append the normal to the stats
-  df_stats_final <- dplyr::full_join(df_stats_all, df_hist_SWE, by = c("station_id", "m_d")) %>%
+  df_stats_final <- dplyr::full_join(df_stats_all, df_hist_SWE, by = c("id", "m_d")) %>%
     dplyr::rename(historic_swe = data)
     #dplyr::mutate(`Number of years within norm range with >= 80% data` = numberofyears_80)
 
