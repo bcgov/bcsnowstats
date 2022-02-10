b2d2e8bdc10961f2715d3e800bd6c8e274fc4811
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-02-09 16:16:33 -0800

    changed to work with new bcsnowdata

--- R/basin_normal.R
+++ R/basin_normal.R
@@ -24,20 +24,20 @@ basin_normal <- function(data, normal_max, normal_min) {
   # Subset the data by the normal max and normal min time
   b_data <- data %>%
     dplyr::filter(lubridate::year(date_utc) >= normal_min & lubridate::year(date_utc) <= normal_max) %>%
-    dplyr::group_by(station_id, m_d)
+    dplyr::group_by(id, m_d)
 
   # Calculate the normal statistics for each day of the year
   b_norm <- do.call(data.frame,
-                        list(dplyr::summarise(b_data, normal_minimum = min(mean_day, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_swe_mean = mean(mean_day, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_Q5 = quantile(mean_day, 0.05, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_Q10 = quantile(mean_day, 0.1, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_Q25 = quantile(mean_day, 0.25, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_Q50 = quantile(mean_day, 0.5, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_Q75 = quantile(mean_day,0.75, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_Q90 = quantile(mean_day,0.90, na.rm = TRUE), .groups = "keep"),
-                             dplyr::summarise(b_data, normal_maximum = max(mean_day, na.rm = TRUE), .groups = "keep"))) %>%
-    dplyr::select(-station_id.1, -station_id.2, -station_id.3, -station_id.4, -station_id.5, -station_id.6, -station_id.7, -station_id.8) %>%
+                        list(dplyr::summarise(b_data, normal_minimum = min(values_stats, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_swe_mean = mean(values_stats, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_Q5 = quantile(values_stats, 0.05, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_Q10 = quantile(values_stats, 0.1, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_Q25 = quantile(values_stats, 0.25, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_Q50 = quantile(values_stats, 0.5, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_Q75 = quantile(values_stats, 0.75, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_Q90 = quantile(values_stats, 0.90, na.rm = TRUE), .groups = "keep"),
+                             dplyr::summarise(b_data, normal_maximum = max(values_stats, na.rm = TRUE), .groups = "keep"))) %>%
+    dplyr::select(-id.1, -id.2, -id.3, -id.4, -id.5, -id.6, -id.7, -id.8) %>%
     dplyr::select(-m_d.1, -m_d.2, -m_d.3, -m_d.4, -m_d.5, -m_d.6, -m_d.7, -m_d.8) %>%
     #dplyr::mutate(Data_Range_normal = (paste0(round(normal_minimum, digits = 0), ' to ', round(normal_maximum, digits = 0)))) %>%
     dplyr::mutate(data_range_normal = (paste0(min(lubridate::year(data$date_utc), na.rm = TRUE), " to ", max(lubridate::year(data$date_utc), na.rm = TRUE)))) %>%
@@ -48,17 +48,17 @@ basin_normal <- function(data, normal_max, normal_min) {
     dplyr::ungroup() %>%
     dplyr::group_by(m_d) %>%
     dplyr::slice(which.min(date_utc)) %>%
-    dplyr::select(date_utc, station_id) %>%
+    dplyr::select(date_utc, id) %>%
     dplyr::rename(date_min_normal_utc = date_utc)
 
   max_date <- data %>%
     dplyr::ungroup() %>%
     dplyr::group_by(m_d) %>%
     dplyr::slice(which.max(date_utc)) %>%
-    dplyr::select(date_utc, station_id) %>%
+    dplyr::select(date_utc, id) %>%
     dplyr::rename(date_max_normal_utc = date_utc)
 
   # append to data
-  dates <- dplyr::full_join(min_date, max_date, by = c("station_id", "m_d"))
-  df_normals_out <- dplyr::full_join(b_norm, dates, by = c("station_id", "m_d"))
+  dates <- dplyr::full_join(min_date, max_date, by = c("id", "m_d"))
+  df_normals_out <- dplyr::full_join(b_norm, dates, by = c("id", "m_d"))
 }
