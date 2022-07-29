fc9a447c88902e8cf4eb25f3e5f4f4285754640c
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-07-29 15:51:21 -0700

    small changes - commented out plots

--- R/manual_2aswe_function.R
+++ R/manual_2aswe_function.R
@@ -49,11 +49,11 @@ manual_2aswe <- function(id, normal_max, normal_min) {
       dplyr::select(Date, swe_mm, station_type)
 
     # Plot the two together as time series
-    plot_a <- ggplot(data = all, aes(x = Date, y = swe_mm, colour = station_type)) +
-      geom_point() +
-      ggtitle(paste0("ASWE and Manual Timeseries: ", id)) +
-      ylab("SWE (mm)") +
-      xlab("Date")
+    #plot_a <- ggplot(data = all, aes(x = Date, y = swe_mm, colour = station_type)) +
+    #  geom_point() +
+    #  ggtitle(paste0("ASWE and Manual Timeseries: ", id)) +
+    #  ylab("SWE (mm)") +
+    #  xlab("Date")
 
     # Plot one against each other
     all_xy <- dplyr::full_join(manual_daily %>%
@@ -65,15 +65,15 @@ manual_2aswe <- function(id, normal_max, normal_min) {
       dplyr::filter(!is.na(aswe_swe), !is.na(manual_swe)) %>%
       dplyr::mutate(month = as.character(lubridate::month(Date)))
 
-    plot_b <- ggplot(data = all_xy, aes(x = manual_swe, y = aswe_swe)) +
-      geom_point(colour = all_xy$month) +
-      geom_smooth(method = "lm", se = FALSE) +
-      ggtitle(paste0("ASWE vs. Manual: ", id)) +
-      ylab("ASWE SWE (mm)") +
-      xlab("Manual SWE (mm)")
+    #plot_b <- ggplot(data = all_xy, aes(x = manual_swe, y = aswe_swe)) +
+    #  geom_point(colour = all_xy$month) +
+    #  geom_smooth(method = "lm", se = FALSE) +
+    #  ggtitle(paste0("ASWE vs. Manual: ", id)) +
+    #  ylab("ASWE SWE (mm)") +
+    #  xlab("Manual SWE (mm)")
 
     # Check to see how many years of overlap
-    # If there is overlap in data, run regression
+    # If there is overlap in data, run regression. Otherwise, do not calculate normals
     if (dim(all_xy)[1] > 0) {
 
       # Get regression statistics
@@ -116,66 +116,63 @@ manual_2aswe <- function(id, normal_max, normal_min) {
           dplyr::select(Date, survey_period, swe_out, data_flag)
       }
 
+      # Check to see if data filling created at record of at least 10 years.
+      years_filled <- missing_aswe_s %>%
+        dplyr::group_by(survey_period) %>%
+        dplyr::full_join(missing_aswe_s) %>%
+        # filter by the normal range
+        dplyr::mutate(wr = bcsnowdata::wtr_yr(Date)) %>%
+        dplyr::filter(wr <= normal_max, wr >= normal_min) %>% # Filter by the normal dates that you specify
+        dplyr::group_by(survey_period) %>%
+        dplyr::filter(!is.na(swe_out))
 
+      # Calculate a normal for the survey period with at least 10 years of data
+      years_filled_10 <- years_filled %>%
+        dplyr::group_by(survey_period) %>%
+        dplyr::summarize(number_years = length(swe_out)) %>%
+        dplyr::full_join(years_filled) %>%
+        dplyr::filter(number_years >= 10) %>%
+        dplyr::group_by(survey_period)
+
+      # Calculate the normal statistics for each day of the year
+      df_normals <- do.call(data.frame,
+                            list(dplyr::summarise(years_filled_10, normal_minimum = min(swe_out, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_swe_mean = mean(swe_out, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_Q5 = quantile(swe_out, 0.05, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_Q10 = quantile(swe_out, 0.1, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_Q25 = quantile(swe_out, 0.25, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_Q50 = quantile(swe_out, 0.5, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_Q75 = quantile(swe_out, 0.75, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_Q90 = quantile(swe_out, 0.90, na.rm = TRUE), .groups = "keep"),
+                                 dplyr::summarise(years_filled_10, normal_maximum = max(swe_out, na.rm = TRUE), .groups = "keep"))) %>%
+        dplyr::select(-survey_period.1, -survey_period.2, -survey_period.3, -survey_period.4, -survey_period.5, -survey_period.6, -survey_period.7, -survey_period.8) %>%
+        #dplyr::mutate(Data_Range_normal = (paste0(round(normal_minimum, digits = 0), ' to ', round(normal_maximum, digits = 0)))) %>%
+        dplyr::mutate(data_range_normal = (paste0(normal_min, " to ", normal_max))) %>%
+        dplyr::mutate(normal_datarange_estimated = paste0(min(lubridate::year(years_filled_10$Date), na.rm = TRUE), " to ", max(lubridate::year(years_filled_10$Date), na.rm = TRUE))) %>%
+        dplyr::mutate(normal_datarange_raw = paste0(aswe_d_min, " to ", aswe_d_max)) %>%
+        dplyr::full_join(years_filled_10 %>% dplyr::select(survey_period, number_years)) %>%
+        dplyr::rename(numberofyears_estimated_80 = number_years)
+
+      # get the day of the max and min!! Use only 'real', non estimated data
+      min_date <- years_filled_10 %>%
+        dplyr::group_by(survey_period) %>%
+        dplyr::slice(which.min(swe_out)) %>%
+        dplyr::select(Date, survey_period) %>%
+        dplyr::rename(date_min_normal_utc = Date)
+
+      max_date <- years_filled_10 %>%
+        dplyr::group_by(survey_period) %>%
+        dplyr::slice(which.max(swe_out)) %>%
+        dplyr::select(Date, survey_period) %>%
+        dplyr::rename(date_max_normal_utc = Date)
+
+      dates <- dplyr::full_join(min_date, max_date)
+      df_normals_out <- dplyr::full_join(df_normals, dates) %>%
+        dplyr::mutate(data_flag = "<10 years data; filled from manual data") %>%
+        dplyr::mutate(m_d = format(survey_period, format = "%m-%d"))
     } else {
-      missing_aswe_s <- NA
-
+      df_normals_out <- NA
     }
-
-    # Check to see if data filling created at record of at least 10 years.
-    years_filled <- missing_aswe_s %>%
-      dplyr::group_by(survey_period) %>%
-      dplyr::full_join(missing_aswe_s) %>%
-      # filter by the normal range
-      dplyr::mutate(wr = bcsnowdata::wtr_yr(Date)) %>%
-      dplyr::filter(wr <= normal_max, wr >= normal_min) %>% # Filter by the normal dates that you specify
-      dplyr::group_by(survey_period) %>%
-      dplyr::filter(!is.na(swe_out))
-
-    # Calculate a normal for the survey period with at least 10 years of data
-    years_filled_10 <- years_filled %>%
-      dplyr::group_by(survey_period) %>%
-      dplyr::summarize(number_years = length(swe_out)) %>%
-      dplyr::full_join(years_filled) %>%
-      dplyr::filter(number_years >= 10) %>%
-      dplyr::group_by(survey_period)
-
-    # Calculate the normal statistics for each day of the year
-    df_normals <- do.call(data.frame,
-                          list(dplyr::summarise(years_filled_10, normal_minimum = min(swe_out, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_swe_mean = mean(swe_out, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_Q5 = quantile(swe_out, 0.05, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_Q10 = quantile(swe_out, 0.1, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_Q25 = quantile(swe_out, 0.25, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_Q50 = quantile(swe_out, 0.5, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_Q75 = quantile(swe_out, 0.75, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_Q90 = quantile(swe_out, 0.90, na.rm = TRUE), .groups = "keep"),
-                               dplyr::summarise(years_filled_10, normal_maximum = max(swe_out, na.rm = TRUE), .groups = "keep"))) %>%
-      dplyr::select(-survey_period.1, -survey_period.2, -survey_period.3, -survey_period.4, -survey_period.5, -survey_period.6, -survey_period.7, -survey_period.8) %>%
-      #dplyr::mutate(Data_Range_normal = (paste0(round(normal_minimum, digits = 0), ' to ', round(normal_maximum, digits = 0)))) %>%
-      dplyr::mutate(data_range_normal = (paste0(normal_min, " to ", normal_max))) %>%
-      dplyr::mutate(normal_datarange_estimated = paste0(min(lubridate::year(years_filled_10$Date), na.rm = TRUE), " to ", max(lubridate::year(years_filled_10$Date), na.rm = TRUE))) %>%
-      dplyr::mutate(normal_datarange_raw = paste0(aswe_d_min, " to ", aswe_d_max)) %>%
-      dplyr::full_join(years_filled_10 %>% dplyr::select(survey_period, number_years)) %>%
-      dplyr::rename(numberofyears_estimated_80 = number_years)
-
-    # get the day of the max and min!! Use only 'real', non estimated data
-    min_date <- years_filled_10 %>%
-      dplyr::group_by(survey_period) %>%
-      dplyr::slice(which.min(swe_out)) %>%
-      dplyr::select(Date, survey_period) %>%
-      dplyr::rename(date_min_normal_utc = Date)
-
-    max_date <- years_filled_10 %>%
-      dplyr::group_by(survey_period) %>%
-      dplyr::slice(which.max(swe_out)) %>%
-      dplyr::select(Date, survey_period) %>%
-      dplyr::rename(date_max_normal_utc = Date)
-
-    dates <- dplyr::full_join(min_date, max_date)
-    df_normals_out <- dplyr::full_join(df_normals, dates) %>%
-      dplyr::mutate(data_flag = "<10 years data; filled from manual data")
-
   } else {
     df_normals_out <- NA
   }
